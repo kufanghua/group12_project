c07a44023d32686d13a6468dd0f525f23dcad5de
yezh0915 <y1590263748@gmail.com>
2025-06-02 16:13:45 +0800

    Update game_manager.py

--- src/game/game_manager.py
+++ src/game/game_manager.py
@@ -1,33 +1,48 @@
 import pygame
 from src.game.map_manager import MapManager
 from src.game.wave_manager import WaveManager
-from src.entities.towers.base_tower import BaseTower
-from src.entities.enemies.base_enemy import BaseEnemy
-from src.entities.projectiles.base_projectile import BaseProjectile
 from src.ui.game_ui import GameUI
-from src.utils.constants import INIT_MONEY, INIT_LIFE, BG_COLOR
+from src.utils.constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 class GameManager:
-    def __init__(self, screen):
-        self.screen = screen
-        self.entities = pygame.sprite.LayeredUpdates()
+    def __init__(self):
+        # 遊戲狀態
+        self.money = 300
+        self.life = 20
+        self.score = 0
+
+        # Sprite Groups
         self.towers = pygame.sprite.Group()
         self.enemies = pygame.sprite.Group()
         self.projectiles = pygame.sprite.Group()
+
+        # 遊戲模組
         self.map_manager = MapManager(self)
         self.wave_manager = WaveManager(self)
         self.ui = GameUI(self)
-        self.money = INIT_MONEY
-        self.life = INIT_LIFE
-        self.score = 0
-        self.selected_tower_type = None
-        self.game_over = False
+
+        self.selected_tower_type = None  # 準備建造的塔型
+        self.running = True
+
+    def update(self, dt):
+        self.wave_manager.update(dt)
+        self.enemies.update(dt)
+        self.towers.update(dt)
+        self.projectiles.update(dt)
+        self.ui.update(dt)
+
+    def draw(self, surface):
+        self.map_manager.draw(surface)
+        self.towers.draw(surface)
+        self.enemies.draw(surface)
+        self.projectiles.draw(surface)
+        self.ui.draw(surface)
 
     def handle_event(self, event):
         if self.ui.handle_event(event):
             return
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:  # left click
+            if event.button == 1:  # 左鍵
                 pos = pygame.mouse.get_pos()
                 if self.selected_tower_type:
                     placed = self.map_manager.place_tower(pos, self.selected_tower_type)
@@ -36,59 +51,26 @@ class GameManager:
                         self.selected_tower_type = None
                 else:
                     self.ui.handle_click(pos)
-            elif event.button == 3:  # right click
+            elif event.button == 3:  # 右鍵
                 self.selected_tower_type = None
+                self.ui.selected_tower = None
 
-    def update(self, dt):
-        if self.game_over:
-            return
-        self.wave_manager.update(dt)
-        self.entities.update(dt)
-        self.check_collisions()
-        self.ui.update(dt)
-        if self.life <= 0:
-            self.game_over = True
-
-    def draw(self):
-        self.screen.fill(BG_COLOR)
-        self.map_manager.draw(self.screen)
-        self.entities.draw(self.screen)
-        self.ui.draw(self.screen)
-
-    def check_collisions(self):
-        # 投射物擊中敵人
-        for projectile in self.projectiles:
-            hits = pygame.sprite.spritecollide(projectile, self.enemies, False)
-            for enemy in hits:
-                projectile.on_hit(enemy)
-                if not projectile.piercing:
-                    projectile.kill()
-        # 敵人到達終點
-        for enemy in self.enemies:
-            if self.map_manager.is_enemy_at_end(enemy):
-                self.life -= 1
-                enemy.kill()
-
-    def add_tower(self, tower: BaseTower):
+    def add_tower(self, tower):
         self.towers.add(tower)
-        self.entities.add(tower)
 
-    def add_enemy(self, enemy: BaseEnemy):
+    def add_enemy(self, enemy):
         self.enemies.add(enemy)
-        self.entities.add(enemy)
 
-    def add_projectile(self, projectile: BaseProjectile):
+    def add_projectile(self, projectile):
         self.projectiles.add(projectile)
-        self.entities.add(projectile)
 
-    def earn_money(self, amount):
-        self.money += amount
+    def lose_life(self, amount=1):
+        self.life -= amount
+        if self.life <= 0:
+            self.running = False
 
-    def add_score(self, val):
-        self.score += val
-
-    def is_game_over(self):
-        return self.game_over
-
-    def get_final_score(self):
-        return self.score
+    def get_tower_at(self, pos):
+        for tower in self.towers:
+            if tower.rect.collidepoint(pos):
+                return tower
+        return None

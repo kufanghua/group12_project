c50d6ae06dda4d2ec1d660b7e1efd30f7f260061
yezh0915 <y1590263748@gmail.com>
2025-06-02 16:29:47 +0800

    Update freeze_tower.py

--- src/entities/towers/freeze_tower.py
+++ src/entities/towers/freeze_tower.py
@@ -3,40 +3,20 @@ from src.entities.towers.base_tower import BaseTower
 from src.entities.projectiles.ice_ball import IceBall
 
 class FreezeTower(BaseTower):
-    name = "Freeze"
+    name = "Freeze Tower"
     cost = 120
+    range = 90
+    attack_speed = 2.0
+    damage = 5
 
     def __init__(self, x, y, game_manager):
+        image = pygame.Surface((36, 36), pygame.SRCALPHA)
+        pygame.draw.circle(image, (100, 200, 255), (18, 18), 17)
+        pygame.draw.rect(image, (60, 180, 255), (10, 26, 16, 6))
         super().__init__(x, y, game_manager)
-        self.image = pygame.Surface((38, 38), pygame.SRCALPHA)
-        pygame.draw.rect(self.image, (140, 140, 255), (6, 6, 26, 26))
-        pygame.draw.circle(self.image, (100, 200, 255), (19, 19), 16, 4)
-        self.range = 95
-        self.damage = 12
-        self.fire_rate = 1.8
-        self.level = 1
-        self.upgrade_cost = 90
-        self.slow_effect = 0.4
-        self.slow_time = 2.2
+        self.image = image
+        self.rect = self.image.get_rect(center=(x, y))
 
     def shoot(self, target):
-        bullet = IceBall(self.x, self.y, target, self.damage, self.game_manager)
-        self.game_manager.projectiles.add(bullet)
-
-    def upgrade(self):
-        if self.level == 1:
-            self.level = 2
-            self.damage += 8
-            self.range += 10
-            self.fire_rate *= 1.10
-            self.slow_effect += 0.1
-            self.upgrade_cost = 140
-            pygame.draw.circle(self.image, (180, 220, 255), (19, 19), 10, 2)
-        elif self.level == 2:
-            self.level = 3
-            self.damage += 15
-            self.range += 10
-            self.fire_rate *= 1.10
-            self.slow_effect += 0.1
-            self.upgrade_cost = 180
-            pygame.draw.rect(self.image, (180, 220, 255), (12, 12, 14, 14))
+        ice_ball = IceBall(self.x, self.y, target, self.damage, self.game_manager)
+        self.game_manager.add_projectile(ice_ball)

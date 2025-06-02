11ea2742ae5b2bc94b23fec62c52d1749f7dd86c
yezh0915 <y1590263748@gmail.com>
2025-06-02 16:31:34 +0800

    Update game_manager.py

--- src/game/game_manager.py
+++ src/game/game_manager.py
@@ -1,51 +1,94 @@
 import pygame
-from src.utils.constants import TILE_SIZE, MAP_ROWS, MAP_COLS, PATH_TILES, TOWER_SPOTS, MAP_BG_COLOR
+from src.game.map_manager import MapManager
+from src.game.wave_manager import WaveManager
+from src.entities.towers.base_tower import BaseTower
+from src.entities.enemies.base_enemy import BaseEnemy
+from src.entities.projectiles.base_projectile import BaseProjectile
+from src.ui.game_ui import GameUI
+from src.utils.constants import INIT_MONEY, INIT_LIFE, BG_COLOR
 
-class MapManager:
-    def __init__(self, game_manager):
-        self.game_manager = game_manager
-        self.grid = [[0 for _ in range(MAP_COLS)] for _ in range(MAP_ROWS)]
-        self.tower_spots = TOWER_SPOTS
-        self.path_tiles = PATH_TILES
+class GameManager:
+    def __init__(self, screen):
+        self.screen = screen
+        self.entities = pygame.sprite.LayeredUpdates()
+        self.towers = pygame.sprite.Group()
+        self.enemies = pygame.sprite.Group()
+        self.projectiles = pygame.sprite.Group()
+        self.map_manager = MapManager(self)
+        self.wave_manager = WaveManager(self)
+        self.ui = GameUI(self)
+        self.money = INIT_MONEY
+        self.life = INIT_LIFE
+        self.score = 0
+        self.selected_tower_type = None
+        self.game_over = False
 
-    def draw(self, surface):
-        # 畫地圖底色
-        surface.fill(MAP_BG_COLOR)
-        # 畫格線
-        for row in range(MAP_ROWS):
-            for col in range(MAP_COLS):
-                pygame.draw.rect(
-                    surface, (200, 200, 200),
-                    (col*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE),
-                    1
-                )
-        # 畫路徑
-        for (row, col) in self.path_tiles:
-            pygame.draw.rect(
-                surface, (180, 180, 100),
-                (col*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE)
-            )
-        # 畫可建塔格
-        for (row, col) in self.tower_spots:
-            pygame.draw.rect(
-                surface, (100, 200, 100),
-                (col*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE)
-            )
+    def handle_event(self, event):
+        if self.ui.handle_event(event):
+            return
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:  # left click
+                pos = pygame.mouse.get_pos()
+                if self.selected_tower_type:
+                    placed = self.map_manager.place_tower(pos, self.selected_tower_type)
+                    if placed:
+                        self.money -= self.selected_tower_type.cost
+                        self.selected_tower_type = None
+                else:
+                    self.ui.handle_click(pos)
+            elif event.button == 3:  # right click
+                self.selected_tower_type = None
 
-    def place_tower(self, pos, tower_type):
-        col, row = pos[0] // TILE_SIZE, pos[1] // TILE_SIZE
-        if (row, col) in self.tower_spots:
-            for tower in self.game_manager.towers:
-                tcol = tower.rect.x // TILE_SIZE
-                trow = tower.rect.y // TILE_SIZE
-                if (trow, tcol) == (row, col):
-                    return False  # 已有塔
-            if self.game_manager.money >= tower_type.cost:
-                tower = tower_type(col*TILE_SIZE+TILE_SIZE//2, row*TILE_SIZE+TILE_SIZE//2, self.game_manager)
-                self.game_manager.add_tower(tower)
-                return True
-        return False
+    def update(self, dt):
+        if self.game_over:
+            return
+        self.wave_manager.update(dt)
+        self.entities.update(dt)
+        self.check_collisions()
+        self.ui.update(dt)
+        if self.life <= 0:
+            self.game_over = True
 
-    def is_enemy_at_end(self, enemy):
-        # 判斷敵人是否到地圖終點
-        return enemy.get_map_grid_pos() == self.path_tiles[-1]
+    def draw(self):
+        self.screen.fill(BG_COLOR)
+        self.map_manager.draw(self.screen)
+        self.entities.draw(self.screen)
+        self.ui.draw(self.screen)
+
+    def check_collisions(self):
+        # 投射物擊中敵人
+        for projectile in self.projectiles:
+            hits = pygame.sprite.spritecollide(projectile, self.enemies, False)
+            for enemy in hits:
+                projectile.on_hit(enemy)
+                if not projectile.piercing:
+                    projectile.kill()
+        # 敵人到達終點
+        for enemy in self.enemies:
+            if self.map_manager.is_enemy_at_end(enemy):
+                self.life -= 1
+                enemy.kill()
+
+    def add_tower(self, tower: BaseTower):
+        self.towers.add(tower)
+        self.entities.add(tower)
+
+    def add_enemy(self, enemy: BaseEnemy):
+        self.enemies.add(enemy)
+        self.entities.add(enemy)
+
+    def add_projectile(self, projectile: BaseProjectile):
+        self.projectiles.add(projectile)
+        self.entities.add(projectile)
+
+    def earn_money(self, amount):
+        self.money += amount
+
+    def add_score(self, val):
+        self.score += val
+
+    def is_game_over(self):
+        return self.game_over
+
+    def get_final_score(self):
+        return self.score
